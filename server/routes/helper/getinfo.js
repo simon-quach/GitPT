3c9621a8ed8da739daa9559ba2cdaa5ad406f4ef
Wilson Lau <wilsonlau@169-231-17-157.wireless.ucsb.edu>
2023-05-13 12:46:37 -0700

    added getinfo.js

---
+++ server/routes/helper/getinfo.js
@@ -0,0 +1,18 @@
+function extractNameAndProject(url){
+    const regex = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)/;
+    const match = url.match(regex);
+
+    if (match ){
+        const namee = match[1];
+        const project = match[2];
+
+        return {
+        namee,
+        project
+        };
+    } else {
+        throw new Error('Invalid Github URL');
+    }
+}
+
+module.exports = extractNameAndProject;
