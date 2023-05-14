ccb442fbd884e0fc74481b6a35794afdf2820fac
wilson <wilsonn.lauu@gmail.com>
2023-05-13 18:52:21 -0700

    fixed getcommitSHA

--- server/routes/helper/returntree.js
+++ server/routes/helper/returntree.js
@@ -1,28 +1,24 @@
-const getCommitSHA = async (owner, repo) => {
-    try {
-      const response = await octokit.repos.getBranch({
-        owner: owner,
-        repo: repo,
-        branch: "main",
-      });
-  
-      const mainBranch = response.data;
-      const commitSHA = mainBranch.commit.sha;
-      console.log("Commit SHA of the main branch:", commitSHA);
-    } catch (error) {
-      console.error("Error:", error);
-    }
+const getCommitSHA = async (octokit, owner, repo) => {
+  const response = await octokit.repos.getBranch({
+    owner: owner,
+    repo: repo,
+    branch: "main",
+  });
+  const mainBranch = response.data;
+  const commitSHA = mainBranch.commit.sha;
+  console.log("Commit SHA of the main branch:", commitSHA);
+  return commitSHA;
+};
 
-    return commitSHA;
-  };
-  
+const getTreeSHA = async (commitSHA) => {
+  const treeSHA = commitSHA.tree.sha;
 
-const getTreeSHA = async(commitSHA) => {
-    const treeSHA = commitSHA.tree.sha;
-    return treeSHA;
-}
+  return treeSHA;
+};
 
-const getTreeObject = async(treeSHA) => {
-    const treeObject = treeSHA.data;
-    return treeObject;
-}
+const getTreeObject = async (treeSHA) => {
+  const treeObject = treeSHA.data;
+  return treeObject;
+};
+
+module.exports = { getCommitSHA, getTreeSHA, getTreeObject };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,88 +1,106 @@
-var express = require('express')
-var router = express.Router()
-var cors = require('cors')
-var dotenv = require('dotenv')
-dotenv.config()
+var express = require("express");
+var router = express.Router();
+var cors = require("cors");
+var dotenv = require("dotenv");
+dotenv.config();
 
-const extractNameAndProject = require('./helper/getinfo.js')
-const summarize = require('./helper/summarize.js')
-const embedding = require('./helper/embedding.js')
-const connectDB = require('./mongo/connect.js')
-
+const extractNameAndProject = require("./helper/getinfo.js");
+const summarize = require("./helper/summarize.js");
+const embedding = require("./helper/embedding.js");
+const connectDB = require("./mongo/connect.js");
+const { getCommitSHA } = require("./helper/returntree.js");
 // MongoDB Setup
-connectDB(process.env.MONGODB_URL)
+connectDB(process.env.MONGODB_URL);
 
 // Middleware
-router.use(express.json())
-router.use(cors())
+router.use(express.json());
+router.use(cors());
 
 // Milvus Database Setup
-const {MilvusClient, DataType, MetricType} = require('@zilliz/milvus2-sdk-node')
-const config = require('../config.js')
-const {uri, user, password, secure} = config
-const milvusClient = new MilvusClient(uri, secure, user, password, secure)
+const {
+  MilvusClient,
+  DataType,
+  MetricType,
+} = require("@zilliz/milvus2-sdk-node");
+const config = require("../config.js");
+const { uri, user, password, secure } = config;
+const milvusClient = new MilvusClient(uri, secure, user, password, secure);
 
 // OpenAI API
-const {Configuration, OpenAIApi} = require('openai')
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_TOKEN,
-})
-const openai = new OpenAIApi(configuration)
+});
+const openai = new OpenAIApi(configuration);
 
 // Github API
-const {Octokit} = require('@octokit/rest')
-const {connect} = require('mongoose')
+const { Octokit } = require("@octokit/rest");
+const { connect } = require("mongoose");
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
-})
+});
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express'})
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
 /* POST request to generate embedding */
-router.post('/embed', async function (req, res, next) {
+router.post("/embed", async function (req, res, next) {
   try {
-    const data = req.body.text
-    const embed = await embedding(openai, data)
-    res.json({embedding: embed})
+    const data = req.body.text;
+    const embed = await embedding(openai, data);
+    res.json({ embedding: embed });
   } catch (error) {
-    res.status(500).json({error: error.toString()})
+    res.status(500).json({ error: error.toString() });
   }
-})
+});
 
-router.post('/github', (req, res) => {
+router.post("/github", (req, res) => {
   try {
-    const url = req.body.url // Assuming the URL is passed in the request body
-    console.log(url)
+    const url = req.body.url; // Assuming the URL is passed in the request body
+    console.log(url);
     // Call the extractNameAndProject function
-    const result = extractNameAndProject(url)
+    const result = extractNameAndProject(url);
 
     // Send the result as the response
-    res.json(result)
+    res.json(result);
   } catch (error) {
     // Handle the error if an invalid GitHub URL is provided
-    res.status(400).json({error: 'Invalid GitHub URL'})
+    res.status(400).json({ error: "Invalid GitHub URL" });
   }
-})
+});
 
-router.post('/summarize', async (req, res) => {
+router.post("/summarize", async (req, res) => {
   try {
-    const owner = req.body.owner // Assuming the owner is passed in the request body
-    const repo = req.body.repo // Assuming the repo is passed in the request body
-    const path = req.body.path // Assuming the path is passed in the request body
+    const owner = req.body.owner; // Assuming the owner is passed in the request body
+    const repo = req.body.repo; // Assuming the repo is passed in the request body
+    const path = req.body.path; // Assuming the path is passed in the request body
 
     // Call the summarize function
-    const summary = await summarize(openai, octokit, owner, repo, path)
+    const summary = await summarize(openai, octokit, owner, repo, path);
 
     // Send the summary as the response
-    res.send(summary)
+    res.send(summary);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     // Handle any errors that occurred during the process
-    res.status(500).json({error: 'Internal server error'})
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 
-module.exports = router
+router.post("/commit-sha", async (req, res) => {
+  try {
+    const { owner, repo } = req.body; // Assuming the owner and repo are passed in the request body
+
+    // Call the getCommitSHA function
+    const commitSHA = await getCommitSHA(octokit, owner, repo);
+    // Send the commit SHA as the response
+    res.send(commitSHA);
+  } catch (error) {
+    console.log(error);
+    // Handle any errors that occurred during the process
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+module.exports = router;
